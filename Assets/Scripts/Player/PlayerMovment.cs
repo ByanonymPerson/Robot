ab6060ceb3151133e76e695a9e9c3cfcf05699fe
ByanonymPerson <89976419+ByanonymPerson@users.noreply.github.com>
2022-05-06 17:38:43 +0300

    Test
    Test

---
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    [SerializeField] private int speed = 10;
+    private Rigidbody rb;
+    public bool isGrounded;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);//move
+
+        
+        if (rb.velocity.x < -.01)// flip
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        else if (rb.velocity.x > .01)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+            if (Input.GetButtonDown("Jump") && isGrounded)//jump
+            {
+                isGrounded = false;
+                rb.AddForce(new Vector3(0 , 400 , 0));
+            }
+    }
+
+
+     private void OnCollisionEnter() { //ground
+
+         isGrounded = true;
+        
+    }
+
+   
+}
